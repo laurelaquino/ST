aab94b99e31cd98a6e0db4665a6568fb31bbfa36
Laurel Aquino <LaurelAquino@Laurels-MacBook-Pro-3.local>
2020-04-06 20:18:15 -0400

    basic tests for $

---
+++ src/st1920/automaton/Task4.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import st1920.automaton.RegExpMatcher;
+
+public class Task1 {
+
+    @Test
+    public void test1() {
+        assertTrue(reg.matches("\n", "$"));
+    }
+
+    @Test
+    public void test2() {
+        assertTrue(reg.matches("\r", "$"));
+    }
+
+    @Test
+    public void test3() {
+        assertTrue(reg.matches("\r\n", "$"));
+    }
+}
