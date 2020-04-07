75230cff65ab22adc22459d88bd213926becf93c
Laurel Aquino <LaurelAquino@Laurels-MacBook-Pro-3.local>
2020-04-07 14:53:36 -0400

    Added more quite basic tests to ensure basic functionality

--- src/st1920/automaton/Task4.java
+++ src/st1920/automaton/Task4.java
@@ -4,7 +4,8 @@ import org.junit.Test;
 
 import st1920.automaton.RegExpMatcher;
 
-public class Task1 {
+public class Task4 {
+    private RegExpMatcher reg;
 
     @Test
     public void test1() {
@@ -25,4 +26,45 @@ public class Task1 {
     public void test4() {
         assertTrue(reg.matches("a\naaa\r\n", "(a+$)+"));
     }
+
+    @Test
+    public void test5() {
+        assertTrue(reg.matches("\r\n", "$$"));
+    }
+
+    @Test
+    public void test6() {
+        assertTrue(reg.matches("\r", "."));
+    }
+
+    @Test
+    public void test7() {
+        assertTrue(reg.matches("\n", "."));
+    }
+
+    @Test
+    public void test8() {
+        assertTrue(reg.matches("\r\n", ".."));
+    }
+
+    @Test
+    public void test9() {
+        assertTrue(reg.matches("\r\n\n\n", "\r\n+"));
+    }
+
+    @Test
+    public void test10() {
+        assertFalse(reg.matches("\r\n\r\n\r\n", "\r\n+"));
+    }
+
+    @Test
+    public void test11() {
+        assertFalse(reg.matches("", "$"));
+    }
+
+    @Test
+    public void test12() {
+        assertFalse(reg.matches("a", "$"));
+    }
+
 }
